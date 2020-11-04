7d0580642b90b4fec0e9313d1e6ae36491644a1d
RuTa6 <mailruhma1997@gmail.com>
2020-11-04 13:52:14 +0530

    configured sls url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
     //baseUrl: "http://localhost:3000"
-    baseUrl:"http://localhost:3000/dev-1"
+    //baseUrl:"http://localhost:3000/dev-1"
+    baseUrl: "https:hu2c09snl7.execute-api.ap-south-1.amazonaws.com/dev-1"
 }
